64007d0fc571407f2345c30a3b373e73e37d3266
Crocodile Jonesy <Discord:crocodilejonesy>
2025-01-23 02:05:32 +1100

    Initial Commit of mod, v0.9.8 - This allows you to set the status of raids when they end for whichever reason
    (cherry picked from commit 1c1a06376bebc3381e30bed7eaefd0e8683ad987)

---
+++ Patch.cs
@@ -0,0 +1,123 @@
+﻿using BepInEx.Logging;
+using EFT;
+using EFT.UI.SessionEnd;
+using EFT.Weather;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurableEndRaidStatus
+{
+    //public class LocalGame : BaseLocalGame<EftGamePlayerOwner>, IBotGame
+    //{
+    //    public BotsController BotsController => throw new NotImplementedException();
+    //    public IWeatherCurve WeatherCurve => throw new NotImplementedException();
+    //    public BossSpawnScenario BossSpawnScenario => throw new NotImplementedException();
+
+
+    //    public override void Stop(string profileId, ExitStatus exitStatus, string exitName, float delay = 0f)
+    //    {
+
+
+    //        base.Stop(profileId, newExitStatus, exitName, delay);
+    //    }
+
+    //    public override IEnumerator vmethod_2()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+
+    //[HarmonyPatch]
+    //public class LocalGamePatch
+    //{
+    //    private static LocalGame
+    //}
+
+    [HarmonyPatch(typeof(BaseLocalGame<EftGamePlayerOwner>), nameof(BaseLocalGame<EftGamePlayerOwner>.Stop))]
+    public class EndRaidPatch : ModulePatch
+    {
+        private static ManualLogSource logSource = BepInEx.Logging.Logger.CreateLogSource("EndRaidPatch");
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BaseLocalGame<EftGamePlayerOwner>).GetMethod("Stop", BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix(ref string profileId, ref ExitStatus exitStatus, ref string exitName, ref float delay)
+        {
+
+            // Check the exit status for result and set based what user sets
+            switch (exitStatus)
+            {
+                case ExitStatus.Survived:
+                    exitStatus = EndRaidPlugin.survivedStatus.Value;
+                    break;
+
+                case ExitStatus.Killed:
+                    exitStatus = EndRaidPlugin.killedStatus.Value;
+                    break;
+
+                case ExitStatus.Left:
+                    exitStatus = EndRaidPlugin.leftStatus.Value;
+                    break;
+
+                case ExitStatus.Runner:
+                    exitStatus = EndRaidPlugin.runnerStatus.Value;
+                    break;
+
+                case ExitStatus.MissingInAction:
+                    exitStatus = EndRaidPlugin.miaStatus.Value;
+                    break;
+
+                // No need to worry about transit so do nothing
+                case ExitStatus.Transit:
+                    break;
+            }
+
+            Logger.LogInfo("Sucessfully patched");
+            return true; // Allow the rest of the method to run
+        }
+    }
+
+    //internal class SessionResultExtPatch : ModulePatch
+    //{
+    //    protected override MethodBase GetTargetMethod()
+    //    {
+    //        return typeof(SessionResultExitStatus).GetMethod("Show", BindingFlags.Public | BindingFlags.Instance);
+    //    }
+
+    //    public static void PatchPreFix(Profile activeProfile, GClass1917 lastPlayerState, ESideType side, ref ExitStatus exitStatus, TimeSpan raidTime, ISession session, bool isOnline)
+    //    {
+    //        // Testing
+    //        if (exitStatus == ExitStatus.Survived)
+    //        {
+    //            exitStatus = ExitStatus.Runner;
+    //            Logger.LogInfo("Set exit status to runner (for testing)");
+
+    //        }
+
+    //        // Testing
+    //        if (exitStatus == ExitStatus.Left)
+    //        {
+    //            exitStatus = ExitStatus.Survived;
+    //        }
+
+    //        // Set the exit status of the game to not be MIA
+    //        if (exitStatus == ExitStatus.MissingInAction)
+    //        {
+    //            exitStatus = ExitStatus.Runner;
+    //            Logger.LogInfo("Set MIA status to runner");
+    //        }
+    //    }
+    //}
+}
---
+++ Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using Comfort.Common;
+using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using SPT.Reflection.Utils;
+using UnityEngine;
+
+/*
+ *  enum ExitStatus
+    {
+        Survived = 0,
+        Killed = 1,
+        Left = 2,
+        Runner = 3,
+        MissingInAction = 4,
+        Transit = 5
+    }
+ * 
+ */
+
+
+namespace ConfigurableEndRaidStatus
+{
+    [BepInEx.BepInPlugin("com.crocodilejonesy.configraidstatus", "Configurable Raid Status", "0.9.8")]
+    public class EndRaidPlugin : BaseUnityPlugin
+    {
+        // Options for each of the relevant exit statuses (Except transit)
+        internal static ConfigEntry<ExitStatus> survivedStatus, killedStatus, leftStatus, runnerStatus, miaStatus;
+
+        // Const strings for ease of use
+        private const string m_CategoryRaidSucess = "Sucessful Raid Status Options (NOT IMPLEMENTED)";
+        private const string m_CategoryRaidFailed = "Failed Raid Status Options";
+
+        private void Awake()
+        {
+            // TODO: Survived
+            survivedStatus = Config.Bind(m_CategoryRaidSucess, "Survived Raid Status", ExitStatus.Survived, "Sets the raid status when you extract");
+            runnerStatus = Config.Bind(m_CategoryRaidSucess, "Runner Raid Status", ExitStatus.Runner, "Sets the raid status when you get a \"Run Through\" (Extract too early)");
+            // TODO: runner
+
+            killedStatus = Config.Bind(m_CategoryRaidFailed, "Killed Raid Status", ExitStatus.Killed, "Sets the raid status when the character is killed");
+            leftStatus = Config.Bind(m_CategoryRaidFailed, "Left The Action Raid Status", ExitStatus.Left, "Sets the raid status when you quit through the menu");
+            miaStatus = Config.Bind(m_CategoryRaidFailed, "Missing in Action Raid Status", ExitStatus.MissingInAction, "Sets the raid status when the raid timer runs out");
+
+            //// Initialise harmony and begin patching, otherwise patches won't work
+            //var harmony = new Harmony("com.crocodilejonesy.configraidstatus");
+            //harmony.PatchAll();
+
+            // Enable patching, otherwise code won't run, this is essentially thc calling of the patching code
+            // NOTE: Make sure this is run AFTER the rest of the awake code otherwise no changes will apply
+            new EndRaidPatch().Enable();
+        }
+
+    }
+}
+
